c914695305bc8709261d086e3344c26e4e0f7236
Gerko Vink <g.vink@uu.nl>
2021-11-11 19:40:29 +0100

    add Day 2

--- Day 1/standardized regression.R
+++ Day 1/standardized regression.R
@@ -5,8 +5,9 @@ cor
 # as a linear model
 fit <- anscombe %$% lm(y1 ~ x1)
 fit
+fit %>% summary()
 # as a linear model with standardized parameters
 fit.scaled <- anscombe %$% lm(scale(y1) ~ scale(x1))
-fit.scaled
+fit.scaled %>% summary()
 # is the standardized parameter for x1 equal to pearson's correlation?
 coef(fit.scaled) == cor
